9fcea38767b423be54bf4857d9e4e4aec1beb0a9
caizx <caizx@chanjet.com>
2021-02-24 14:33:21 +0800

    feat: 去掉地区参照，这种简单的数据不需要弹出来参照界面选择

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export { default as RegionEntity } from './entities/RegionEntity';
 export { default as RegionTable } from './tables/RegionTable';
 export { default as RegionEdit } from './views/RegionEdit';
 export { default as RegionList } from './views/RegionList';
-export { default as RegionRefer } from './views/RegionRefer';
